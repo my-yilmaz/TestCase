e35e2eae1a1ee001a3d9613de36ce80d544c5e72
my-yilmaz <myyilmaz046@gmail.com>
2022-06-04 10:26:29 +0300

    test case 02

---
+++ src/test/java/tests/TC02_PozitifLoginTest.java
@@ -0,0 +1,32 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.AutomationExercisePage2;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class TC02_PozitifLoginTest {
+    AutomationExercisePage2 exercisePage2=new AutomationExercisePage2();
+
+    @Test
+    public void test01() {
+        //1. Launch browser
+        //2. Navigate to url 'http://automationexercise.com'
+        Driver.getDriver().get(ConfigReader.getProperty("siteUrl"));
+        //3. Verify that home page is visible successfully
+        Assert.assertTrue(exercisePage2.logoElementi.isDisplayed());
+        //4. Click on 'Signup / Login' button
+        exercisePage2.loginSignUP.click();
+        //5. Verify 'Login to your account' is visible
+        Assert.assertTrue(exercisePage2.logintoYourAcoountText.isDisplayed());
+        //6. Enter correct email address and password
+        exercisePage2.emailBox.sendKeys(ConfigReader.getProperty("email"));
+        exercisePage2.passwordBox.sendKeys(ConfigReader.getProperty("password"));
+        //7. Click 'login' button
+        exercisePage2.loginButonu.click();
+        //8. Verify that 'Logged in as username' is visible
+        Assert.assertTrue(exercisePage2.userProfil.isDisplayed());
+        Driver.closeDriver();
+    }
+}
